7fa0a4614f802dde1e9ddd5c09944510a4790988
FabioSinobre <engenheiro.computacao4.0@gmail.com>
2021-02-28 22:48:06 -0300

    up

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,9 +4,12 @@ package application;
  * @author Fabio D. Sinobre
  *
  */
+import boardgame.Position;
 public class Program {
 
 	public static void main(String[] args) {
+		Position position = new Position(3, 6);
+		System.out.print(position);
 		
 	}
 
